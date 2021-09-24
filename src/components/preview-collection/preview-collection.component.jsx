7aede5155b978d41638c315454072a0cf4acf7f0
Samson T. Oketunmbi <oyattunechi@gmail.com>
2021-09-24 14:00:38 -0600

    feat: updated shop data with new data and also the cart utils to group items in cart data

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -10,8 +10,8 @@ const PreviewCollection = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({id, ...otherItemProps}) => (
-          <ItemCollection key={id} {...otherItemProps} />
+        .map((item) => (
+          <ItemCollection key={item.id} item={item} />
         ))}
     </div>
   </div>
